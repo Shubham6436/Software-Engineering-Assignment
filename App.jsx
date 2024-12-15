0eeaea87b2576133945e3226dbaba760f384560a
Shubham6436 <121788298+Shubham6436@users.noreply.github.com>
2024-12-15 11:12:53 +0530

    Add files via upload
    Task-1

---
+++ App.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import Header from "./Components/Header";
+import Board from "./components/Board";
+import { useDispatch, useSelector } from "react-redux";
+import boardsSlice from "./redux/boardsSlice";
+import EmptyBoard from "./Components/EmptyBoard";
+
+function App() {
+  const dispatch = useDispatch();
+  const boards = useSelector((state) => state.boards);
+  const theme = useSelector((state) => state.theme);
+  const activeBoard = boards.find((board) => board.isActive);
+  if (!activeBoard && boards.length > 0)
+    dispatch(boardsSlice.actions.setBoardActive({ index: 0 }));
+
+  return (
+    <div className={`app ${theme}`}>
+      {boards.length > 0 ? (
+        <>
+          <Header />
+          <Board />
+        </>
+      ) : (
+        <EmptyBoard type="add" />
+      )}
+    </div>
+  );
+}
+
+export default App;
